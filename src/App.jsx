08e7df07560eb38cc16cebbfacdd582abb6695f6
Mohd Salim <saalim9965@gmail.com>
2024-06-30 16:21:38 +0530

    section 4: lecture 73 first step towards tic tac toe game

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ function App() {
   
 
   return (
-    <></>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
   )
 }
 
