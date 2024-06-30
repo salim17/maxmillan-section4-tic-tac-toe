fcb39bb74ebb3563a7b0547b674c92890714edd8
Mohd Salim <saalim9965@gmail.com>
2024-06-30 17:34:01 +0530

    section 4: lecture 79 user input and two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     //do not ever update the state like this, always use anonymous function, because in the background react schedules this update and is not synchronous
-    // setIsEditing(!isEditing); 
+    // setIsEditing(!isEditing);
     // setIsEditing(!isEditing); // try this by having this line twice
-    setIsEditing(editing => !editing); // now try this by having this line twice
-}
+    setIsEditing((editing) => !editing); // now try this by having this line twice
+  }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
